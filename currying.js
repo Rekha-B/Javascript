cdbdcec2ae0c56edb2317b3d335900560c2147ff
Rekha-B <baburekha002691@gmail.com>
2025-11-30 20:20:04 +0530

    Implement currying for the multiply function
    Added currying function to enable partial application of arguments for the multiply function.

--- currying.js
+++ currying.js
@@ -35,3 +35,27 @@ function getProduct(num1) {
   const travelTimeBosNyc = getTravelTime(400);
   const travelTimeMiamiAtlanta = getTravelTime(600);
   console.log(travelTimeBosNyc(100));
+
+
+
+function currying(fn){
+    return function curried(...args){
+        if(args.length >= fn.length){
+            return fn.apply(this,args);
+        }
+        else {
+            return function(...args2){
+                return curried.apply(this, args.concat(args2));
+            }
+        }
+    };
+}
+
+function multiply(a, b, c, d) {
+    return a * b * c * d;
+}
+
+const curriedMultiply = currying(multiply);
+console.log(curriedMultiply(1)(2)(3)(4)); // 24
+console.log(curriedMultiply(1, 2)(3, 4)); // 24
+console.log(curriedMultiply(1)(2, 3)(4)); // 24
